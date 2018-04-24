928ed3ebdcd9c9c91d0e4b83dd62efc6ed254359
Blaise James <blaise@blaisejames.com>
2018-04-24 11:14:55 -0400

    first commit of countdownFours.js

---
+++ countdownFours.js
@@ -0,0 +1,5 @@
+var count = 2016;
+while (count > 0) {
+    console.log(count);
+    count = count - 4;
+}
--- whoa.js
+++ whoa.js
@@ -2,6 +2,6 @@ var sum = 0;
 for (var i = -300000; i <= 300000; i++) {
     if (i % 3 == 0) {
         sum = sum + i;
-    };
-};
+    }
+}
 console.log(sum);
