a8e93ba1cc8de046f2d35b6727e4acb20f7c135f
Blaise James <blaise@blaisejames.com>
2018-04-25 11:32:22 -0400

    second commit of celsiusToFahrenheit.js

--- celsiusToFahrenheit.js
+++ celsiusToFahrenheit.js
@@ -1,6 +1,6 @@
-function fahrenheitToCelsius(cDegrees) {
+function celsiusToFahrenheit(cDegrees) {
     var fahrenheit = 0;
     fahrenheit = cDegrees * 1.8 + 32;
     return cDegrees + " celsius is " + fahrenheit + " fahrenheit";
 }
-console.log(fahrenheitToCelsius(0));
+console.log(celsiusToFahrenheit(0));
