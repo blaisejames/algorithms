1d4ff4d153ddaaa16579ca1e1dba72749846b42e
Blaise James <blaise@blaisejames.com>
2018-04-25 09:27:07 -0400

    first commit of lengthValue.js

---
+++ lengthValue.js
@@ -0,0 +1,11 @@
+function lengthValue(num1, num2) {
+    arr = [];
+    for (i = 0; i < num1; i++) {
+        arr.push(num2);
+    }
+    if (num1 == num2) {
+        console.log("Jinx!");
+    }
+    return arr;
+}
+console.log(lengthValue(6,6));
