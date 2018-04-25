b839dd8b0e8b3a2ee7a54d4a1c2e450674ef6289
Blaise James <blaise@blaisejames.com>
2018-04-25 12:16:57 -0400

    second commit of doubleVision.js

--- doubleVision.js
+++ doubleVision.js
@@ -3,6 +3,7 @@ function double(arr) {
     for (var i = 0; i < arr.length; i++) {
         newarr.push(arr[i] * 2);
     }
+    console.log("Original array: [" + arr + " ]")
     return "New array with doubled values: [ " + newarr + " ]";
 }
 console.log(double([2,4,6]));
