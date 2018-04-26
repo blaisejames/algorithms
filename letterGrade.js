e2e0457b229f055624da5e1e40d981ed15284063
Blaise James <blaise@blaisejames.com>
2018-04-26 11:14:18 -0400

    first commit of letterGrade.js

--- letterGrade.js
+++ letterGrade.js
@@ -1,15 +1,25 @@
 function letterGrade(score) {
-    if (score >=90 && score <=100) {
+    var sign = "";
+    if (score % 10 == 0 || score % 10 == 1 || score % 10 == 2) { 
+        sign = "-";
+        }
+    else if (score % 10 == 8 || score % 10 == 9) { 
+        sign = "+";
+    }
+    if (score >=98 && score <=100) {
         console.log ("Score: " + score + ". Grade: A");
     }
+    else if (score >=90 && score <=97) {
+        console.log ("Score: " + score + ". Grade: A" + sign);
+    }
     else if (score >=80 && score <90) {
-        console.log ("Score: " + score + ". Grade: B");
+        console.log ("Score: " + score + ". Grade: B" + sign);
     }
     else if (score >=70 && score <80) {
-        console.log ("Score: " + score + ". Grade: C");
+        console.log ("Score: " + score + ". Grade: C" + sign);
     }
     else if (score >=60 && score <70) {
-        console.log ("Score: " + score + ". Grade: D");
+        console.log ("Score: " + score + ". Grade: D" + sign);
     }
     else if (score >=0 && score <60) {
         console.log ("Score: " + score + ". Grade: F");
